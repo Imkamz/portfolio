14005cb96d22b7f38deefad0fa19282fa2c03fa4
Kamari L <kamarilee27@gmail.com>
2023-01-23 14:52:42 +0000

    Layout file

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-import Navbar from "@/components/navbar";
-
 export default function Home() {
     return (
         <>
-            <Navbar></Navbar>
+
             <h1>about</h1>
         </>
     )
