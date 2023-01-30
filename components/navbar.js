0435b7901d7ffdf3b6cacef1d1d78f0a9f5e56c6
Kamari L <kamarilee27@gmail.com>
2023-01-30 14:13:43 +0000

    Added Github link

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 import Styles from "../styles/Navbar.module.scss"
 import Link from "next/link"
+import { FaGithub } from "react-icons/fa"
 export default function Navbar() {
     return (
         <nav className={Styles.navbar}>
@@ -11,9 +12,13 @@ export default function Navbar() {
                 </li>
                 <li>
                     <Link href="/about">
-                        about
+                        About
                     </Link>
-
+                </li>
+                <li>
+                    <Link href="https://github.com/Imkamz" target="_blank">
+                        <FaGithub></FaGithub>  Github
+                    </Link>
                 </li>
             </ul>
         </nav>
