b97aa11f0ddede36a38d1c6c3909c7b56ccdf31c
Kamari L <kamarilee27@gmail.com>
2023-03-12 22:19:39 +0000

    home page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,14 @@ export default function Home() {
     <>
       <h1>Kamari Lee</h1>
       <p className='text-center'>Aspiring software developer from London</p>
+      <p className='flex py-5'>I am a career-minded A-Level student with a GSCE record of good grades in different subjects. I work hard on assignments and
+        consistently meet objectives. I want to gain experience outside of a classroom and build foundational knowledge and skills. I am
+        also energetic with a balanced background in academic and extra-curricular activities. I am diligent about preparing for
+        examinations, competitions and assignments. My peers, teachers and classmates, consider me reliable and responsible with
+        good communication and relationship-building abilities. I am a quick learner with academic abilities and technical knowledge to
+        succeed in different roles. I am ready to expand horizons with additional knowledge and abilities gained from training and
+        experience. I will always be ready to help others and use abilities to support team goals.</p>
+
     </>
   )
 }
